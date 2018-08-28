d14f4b69014f69cd1571e8352126bfaef53d131d
chaitanya chintalapati <>
2018-08-28 12:03:34 -0500

    added common-resources and formated files

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -8,21 +8,22 @@ import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
-    @Test
-    public void mavenFlowReturnsHelloMaven() throws Exception {
-        runFlowAndExpect("maven-projectFlow", "Hello Maven");
-    }
-    
-    @Override
-    protected String getConfigFile() {
-        return "maven-project.xml";
-    }
-    
-    @Test
-    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
-    MuleEvent event = runFlow("retrieveFlights");
-    String contentType = event.getMessage().getOutboundProperty("Content-Type");
-    assertEquals("application/json", contentType);
-    }
+	@Test
+	public void mavenFlowReturnsHelloMaven() throws Exception {
+		runFlowAndExpect("maven-projectFlow", "Hello Maven");
+	}
+
+	@Override
+	protected String[] getConfigFiles() {
+		String[] files = { "maven-project.xml", "common-resources.xml" };
+		return files;
+	}
+
+	@Test
+	public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+		MuleEvent event = runFlow("retrieveFlights");
+		String contentType = event.getMessage().getOutboundProperty("Content-Type");
+		assertEquals("application/json", contentType);
+	}
 
 }
